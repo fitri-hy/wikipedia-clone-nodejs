4772454b9553c9eac6a294b50b9a3401c34bec71
fitri-hy <noreply.orzpartners@gmail.com>
2024-10-05 17:04:34 +0700

    update

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
-
-app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 const ITEMS_PER_PAGE = 15;
 const BASE_URL = 'https://id.wikipedia.org/w/api.php';
